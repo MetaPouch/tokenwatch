88c97a6de1785e94fc3b8face54401b5dec1df1a
Ajay Srinivas <20982682+ajays97@users.noreply.github.com>
2026-09-23 20:32:10 +0530

    Bump cask mirror to v1.8.5

--- homebrew-cask/tokenwatch.rb
+++ homebrew-cask/tokenwatch.rb
@@ -1,6 +1,6 @@
 cask "tokenwatch" do
-  version "1.8.4"
-  sha256 "259b2c267c25e497951cab34102b291935dee9709a30221b8a3751bc22ff53f7"
+  version "1.8.5"
+  sha256 "bc547aa7485badae2107d5b6fc2e71bee3a49c104ecb9468fe543a440f6ff909"
 
   url "https://github.com/MetaPouch/tokenwatch/releases/download/v#{version}/TokenWatch-#{version}.dmg"
   name "TokenWatch"
